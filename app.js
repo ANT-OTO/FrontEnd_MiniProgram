56e7446201da712327e274034393e049ee6ced33
MatthewDong <matthewdong19870801@gmail.com>
2018-10-31 14:42:54 -0400

    init

---
+++ app.js
@@ -0,0 +1,36 @@
+var util = require('./utils/util.js');
+var api = require('./config/api.js');
+var user = require('./services/user.js');
+
+App({
+  onLaunch: function () {
+    //获取用户的登录信息
+    user.checkLogin().then(res => {
+      console.log('app login')
+      this.globalData.userInfo = wx.getStorageSync('userInfo');
+      this.globalData.token = wx.getStorageSync('token');
+    }).catch(() => {
+      //1.ask permison to login or register
+      user.loginByWeixin().then(res => {
+        // this.setData({
+        //   userInfo: res.data.userInfo
+        // });
+        this.globalData.userInfo = res.data.userInfo;
+        this.globalData.token = res.data.token;
+      }).catch((err) => {
+        console.log(err)
+      });
+
+    });
+  },
+  
+  globalData: {
+    
+    userInfo: {
+      nickname: 'Hi,游客',
+      username: '点击去登录',
+      avatar: 'http://yanxuan.nosdn.127.net/8945ae63d940cc42406c3f67019c5cb6.png'
+    },
+    token: '',
+  }
+})
---
+++ services/user.js
@@ -0,0 +1,91 @@
+/**
+ * 用户相关服务
+ */
+
+const util = require('../utils/util.js');
+const api = require('../config/api.js');
+
+/**
+ * 调用微信登录
+ */
+function loginByWeixin() {
+
+  let code = null;
+  let userInfo = null;
+  return new Promise(function (resolve, reject) {
+    return util.login().then((res) => {
+      console.log("######################## wx login, get code")
+      code = res.code;
+      return util.getUserInfo();
+    }).then((res) => {
+      console.log("######################## get user info")
+      console.log(res)
+      userInfo = res.userInfo
+      return util.getOpenId(code);
+    }).then((res)=>{
+      console.log("######################## get open id")
+      console.log(res)
+      if (res.data.openid) {
+        var body = {};
+        userInfo.openid = res.data.openid;
+        userInfo.session_key = res.data.session_key;
+        userInfo.code = code;
+        body.ThirdPartyInfo = {
+          "CustomerTypeCodeId": 2,
+          "ThirdPartyId": res.data.openid,
+          "NickName": userInfo.nickName,
+          "AvatarUrl": userInfo.avatarUrl,
+          "Gender": userInfo.gender
+        };
+        return util.loginToANT(body);
+      }
+    }).then((res)=>{
+      console.log("######################### login ")
+      console.log(res);
+      if (res.data && res.data.Token){
+        wx.setStorageSync('userInfo', userInfo);
+        wx.setStorageSync('token', res.data.Token);
+        console.log("######################### login dd ")
+
+      }
+    }).catch((err) => {
+      reject(err);
+    })
+  });
+}
+
+/**
+ * 判断用户是否登录
+ */
+function checkLogin() {
+  return new Promise(function (resolve, reject) {
+    if (wx.getStorageSync('userInfo') && wx.getStorageSync('token')) {
+
+      util.checkSession().then(() => {
+        resolve(true);
+      }).catch(() => {
+        reject(false);
+      });
+
+    } else {
+      reject(false);
+    }
+  });
+}
+
+
+module.exports = {
+  loginByWeixin,
+  checkLogin,
+};
+
+
+
+
+
+
+
+
+
+
+
